c564cd26b777de02cf8255c6b72b7f595cf82cc7
Choidongho08 <157602117+Choidongho08@users.noreply.github.com>
2024-09-18 22:25:38 +0900

    로비 만들기 시작

--- Assets/Scripts/DH/Lobby.cs
+++ Assets/Scripts/DH/Lobby.cs
@@ -1,8 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Services.Core;
 using UnityEngine;
 
 public class Lobby : MonoBehaviour
 {
-
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+    }
 }
