d47794bbd78d6df228eed2ff09b43dff6ab04ccc
Choidongho08 <157602117+Choidongho08@users.noreply.github.com>
2024-09-20 00:51:15 +0900

    자기전 커민ㅅ

--- Assets/Scripts/DH/TestLobby.cs
+++ Assets/Scripts/DH/TestLobby.cs
@@ -10,6 +10,9 @@ using UnityEngine.UI;
 
 public class TestLobby : MonoBehaviour
 {
+    private Lobby hostLobby;
+    private float heartbeatTimer = 15;
+
     private async void Start()
     {
         await UnityServices.InitializeAsync();
@@ -24,9 +27,28 @@ public class TestLobby : MonoBehaviour
 
     private void Update()
     {
+        HandleLobbyHeartbeat();
         if(Input.GetKeyDown(KeyCode.C))
         {
             CreateLobby();
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            ListLobbies();
+        }
+    }
+    private async void HandleLobbyHeartbeat()
+    {
+        if(hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if(heartbeatTimer < 0)
+            {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
         }
     }
 
@@ -35,12 +57,77 @@ public class TestLobby : MonoBehaviour
         try
         {
             string lobbyName = "MyLobby";
+            CreateLobbyOptions createLobbyOptions = new CreateLobbyOptions
+            {
+                IsPrivate = true,
+            };
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, 2, createLobbyOptions);
 
-            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, 2);
+            hostLobby = lobby;
+            Debug.Log($"Created Lobby! {lobby.Name} {lobby.LobbyCode}");
         }
         catch (LobbyServiceException e)
         {
             Debug.Log(e);
         }
+    }
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryLobbiesOptions queryLobbiesOptions = new QueryLobbiesOptions
+            {
+                Count = 25,
+                Filters = new List<QueryFilter>
+                {
+                    new QueryFilter(QueryFilter.FieldOptions.AvailableSlots, "0", QueryFilter.OpOptions.GT),
+                },
+                Order = new List<QueryOrder>
+                {
+                    new QueryOrder(false, QueryOrder.FieldOptions.Created)
+                },
+            };
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync(queryLobbiesOptions);
+
+            Debug.Log($"Lobbies found : {queryResponse.Results.Count}");
+
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log($"{lobby.Name}");
+            }
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+    private async void JoinLobbyByCode(string lobbyCode)
+    {
+        try
+        {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            await Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
+
+            Debug.Log($"joined lobby with code {lobbyCode}");
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+       
+    }
+
+    private async void QuickJoinLobby()
+    {
+        try
+        {
+            await LobbyService.Instance.QuickJoinLobbyAsync();
+        }
+        catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+        
     }
 }
