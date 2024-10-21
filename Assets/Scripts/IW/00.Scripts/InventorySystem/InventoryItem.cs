9ffdee7b368c3678f2dce7db0f1a732718595acc
ggach1 <dlsdnj1234@gmail.com>
2024-10-22 00:45:39 +0900

    inventory script

--- Assets/Scripts/IW/00.Scripts/InventorySystem/InventoryItem.cs
+++ Assets/Scripts/IW/00.Scripts/InventorySystem/InventoryItem.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GetItem : MonoBehaviour
+public class InventoryItem : MonoBehaviour
 {
     
 }
--- Assets/Scripts/IW/00.Scripts/InventorySystem/ItemSO.cs
+++ Assets/Scripts/IW/00.Scripts/InventorySystem/ItemSO.cs
@@ -1,12 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 [CreateAssetMenu(fileName = "ItemSO", menuName = "Item So", order = 0)]
 public class ItemSO : ScriptableObject
 {
     [Header("아이템 정보")]
     public string ItemName; // 아이템의 고유 ID
+    public string ItemInfo; // 아이템 설명
+    public Image ItemIcon; // 아이템 그림
 
     [Header("플레이어 상호작용")]
     public string[] PlayerID;
