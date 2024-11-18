b95de58123adb269a1657b4f76b72c5d74ce8e2d
ggach1 <dlsdnj1234@gmail.com>
2024-11-18 10:42:41 +0900

    inven ReBoot

--- Assets/Scripts/IW/00.Scripts/InventorySystem/Inventory.cs
+++ Assets/Scripts/IW/00.Scripts/InventorySystem/Inventory.cs
@@ -3,21 +3,35 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using System.IO;
+
+[System.Serializable]
+public class InvenData
+{
+    public List<ItemSlotData> slots = new List<ItemSlotData>();
+}
+
+[System.Serializable]
+public class ItemSlotData
+{
+    public string itemName;
+}
 
 public class Inventory : MonoBehaviour
 {
+    private string _filePath;
+
     public static Inventory Instance;
     public static InventoryItem _carriedItem;
 
     private FieldItem _fItem;
 
     [SerializeField] InventorySlot[] _inventorySlots;
-
     [SerializeField] Transform _draggablesTransform;
     [SerializeField] InventoryItem _itemPrefabs;
 
     [SerializeField] Image _icon;
-    [SerializeField] TextMeshProUGUI _Info;
+    [SerializeField] TextMeshProUGUI _info;
 
     [Header("Item List")]
     public ItemSO[] _items;
@@ -26,7 +40,36 @@ public class Inventory : MonoBehaviour
     {
         Instance = this;
         _fItem = GetComponent<FieldItem>();
+
+        _filePath = Application.persistentDataPath + "/Inventory.json";
     }
+
+    private void Start()
+    {
+        // LoadInventory();
+    }
+
+    /*SaveInventoryToJson
+    public void SaveInventory()
+    {
+        File.WriteAllText(_filePath, JsonUtility.ToJson(this));
+        Debug.Log("Saving Data");
+    }
+
+    public Inventory LoadInventory()
+    {
+        if (File.Exists(_filePath))
+        {
+            string jsonData = File.ReadAllText(_filePath);
+            Debug.Log("Reloading Save Data");
+            return JsonUtility.FromJson<Inventory>(jsonData);  
+        }
+        else
+        {
+            Debug.LogError("Save File Not Found");
+            return null;
+        }
+    }*/
 
     // 아이템 잘 생성되는 지
     public void SpawnInventoryItem(ItemSO item = null)
@@ -43,12 +86,14 @@ public class Inventory : MonoBehaviour
             // 슬롯이 비어있는지 체크
             if (_inventorySlots[i]._myItem == null)
             {
-                Debug.Log(itemSO);
                 Instantiate(_itemPrefabs, _inventorySlots[i].transform).Initialize(itemSO, _inventorySlots[i]);
-                Debug.Log("아이테무 생성");
                 _itemPrefabs.GetComponent<RectTransform>().localScale = _inventorySlots[i].transform.localScale;
                 _icon.sprite = item.ItemIcon;
-                _Info.text = item.ItemInfo;
+                _info.text = item.ItemInfo;
+
+                // SaveInventory();
+                Debug.Log(itemSO);
+                ChangeIcon(itemSO);
                 return;
             }
         }
@@ -69,5 +114,24 @@ public class Inventory : MonoBehaviour
         _carriedItem = item;
         _carriedItem._canvasGroup.blocksRaycasts = false;
         item.transform.SetParent(_draggablesTransform);
+    }
+
+    public void ChangeIcon(ItemSO item)
+    {
+        if (item == null || item.ItemIcon == null || item.ItemInfo == null)
+        {
+            Debug.LogError("Invalid item data passed to ChangeIcon.");
+            return;
+        }
+
+        if (_icon == null || _info == null)
+        {
+            Debug.LogError("_icon or _info is not assigned in the Inspector.");
+            return;
+        }
+
+        _icon.sprite = item.ItemIcon;
+        _info.text = item.ItemInfo;
+        Debug.Log("Icon and Info updated for item: " + item.ItemName);
     }
 }
