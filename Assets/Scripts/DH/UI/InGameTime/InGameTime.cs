71de9fb76949f8df87a9e91f295314d123c09672
Choidongho08 <157602117+Choidongho08@users.noreply.github.com>
2024-11-18 09:35:29 +0900

    시간(날짜 지나감)

--- Assets/Scripts/DH/UI/InGameTime/InGameTime.cs
+++ Assets/Scripts/DH/UI/InGameTime/InGameTime.cs
@@ -1,18 +1,35 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InGameTime : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private Image _timeSprite;
+    [SerializeField] private TimeSpriteSO _timeSpriteSo;
+    [SerializeField] private int _minute;
+
+    private int _timeCount = 0;
+
+    private void Start()
     {
-        
+        TimeStart();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void TimeStart()
     {
-        
+        StartCoroutine(TimeCoroutine());
+    }
+    private IEnumerator TimeCoroutine()
+    {
+        yield return new WaitForSecondsRealtime(_minute * 60);
+        _timeCount++;
+        if (_timeCount >= 6)
+            _timeSprite.sprite = _timeSpriteSo.timeSprites[_timeCount = 0];
+        else
+            _timeSprite.sprite = _timeSpriteSo.timeSprites[_timeCount];
+        StartCoroutine(TimeCoroutine());
     }
 }
