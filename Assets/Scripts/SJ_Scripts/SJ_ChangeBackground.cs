e0843bb8ca8a879b9b97c59c1adba6480a73d96a
djEjsdlfma <asdakpol.asd@gmail.com>
2024-11-22 00:07:16 +0900

    feat : StorySO, Main2
    스토리 SO 얼추 만들고 메인2 작업 중간 올림

---
+++ Assets/Scripts/SJ_Scripts/SJ_ChangeBackground.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SJ_ChangeBackground : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> _backgroundList;
+    //[Range(0, 10)]
+}
