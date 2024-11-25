8c18d2436a0d0c01544fe48ff331d230aa72a3d5
ggach1 <dlsdnj1234@gmail.com>
2024-11-25 20:21:46 +0900

    select criminal

--- Assets/Scripts/IW/00.Scripts/LastChance/SelectCriminal.cs
+++ Assets/Scripts/IW/00.Scripts/LastChance/SelectCriminal.cs
@@ -6,62 +6,65 @@ public class SelectCriminal : MonoBehaviour
 {
     [SerializeField] private List<GameObject> _charL;
 
+    public Transform _draggablesTransform;
+    public Transform _lRestTransform;
+    public Transform _rRestTransform;
+
     public int _currentIndex = 0; // 현재 캐릭터 인덱스
+
+    private void Start()
+    {
+        // 초기 위치 설정
+        UpdateCharPosi();
+    }
+
+    private void UpdateCharPosi()
+    {
+        for (int i = 0; i < _charL.Count; i++)
+        {
+            if (i == _currentIndex) // 현재 캐릭터 중앙 배치
+            {
+                _charL[i].transform.position = _draggablesTransform.position;
+                _charL[i].SetActive(true);
+            }
+            else if (i == (_currentIndex - 1 + _charL.Count) % _charL.Count) // 이전 캐릭터 왼쪽 배치
+            {
+                _charL[i].transform.position = _lRestTransform.position;
+                _charL[i].SetActive(true);
+            }
+            else if (i == (_currentIndex + 1) % _charL.Count) // 다음 캐릭터 오른쪽 배치
+            {
+                _charL[i].transform.position = _rRestTransform.position;
+                _charL[i].SetActive(true);
+            }
+            else // 나머지는 캐릭터 화면에서 보이지 않는 위치로
+            {
+                _charL[i].transform.position = _rRestTransform.position; // 화면 밖
+                _charL[i].SetActive(false);
+            }
+        }
+    }
 
     public void MoveLeft()
     {
         Debug.Log("Left");
 
-        if (_currentIndex == 0)
-        {
-            Debug.Log("You couldn't move Left");
-            return;
-        }
-        if (_currentIndex < 0)
-        {
-            _currentIndex = 0;
-            return;
-        }
-
-        if (_currentIndex >= 0 && _currentIndex <= _charL.Count)
-        {
-            _charL[_currentIndex].transform.position = _charL[_currentIndex - 1].transform.position;
-            
-            for (int i = 0; i < _charL.Count; i++)
-            {
-                _charL[i].transform.position = _charL[i - 1].transform.position;
-            }
-
-        }
-
-        _currentIndex--;
+        _currentIndex = (_currentIndex - 1 + _charL.Count) % _charL.Count;
+        UpdateCharPosi();
+        Debug.Log("Current Index : " + _currentIndex);
     }
 
     public void MoveRight()
     {
         Debug.Log("Right");
 
-        if (_currentIndex <= _charL.Count)
-        {
-            Debug.Log("You couldn't move Right");
-            return;
-        }
-        if (_currentIndex >= _charL.Count)
-        {
-            _currentIndex = _charL.Count;
-            return;
-        }
+        _currentIndex = (_currentIndex + 1) % _charL.Count;
+        UpdateCharPosi();
+        Debug.Log("Current Index : " + _currentIndex);
+    }
 
-        if (_currentIndex >= 0 && _currentIndex < _charL.Count)
-        {
-            // _charL[_currentIndex].transform.position = _charL[_currentIndex + 1].transform.position;
-
-            for (int i = 0; i < _charL.Count; i++)
-            {
-                _charL[i].transform.position = _charL[i + 1].transform.position;
-            }
-        }
-
-        _currentIndex++;
+    public void Proposal()
+    {
+        Debug.Log("Select Criminal");
     }
 }
