c53208f3bfe49dc04ff7a1ee162ae5c3ecb17a8f
djEjsdlfma <asdakpol.asd@gmail.com>
2024-11-27 01:51:15 +0900

    feat : EndingSceneFinish
    엔딩 끝냈다 야호

--- Assets/Scripts/SJ_Scripts/RSJ_EndingStr.cs
+++ Assets/Scripts/SJ_Scripts/RSJ_EndingStr.cs
@@ -32,6 +32,7 @@ public class RSJ_EndingStr : MonoBehaviour
         if (_storyLine != _storyList.Count)
         {
             _text.text = _storyList[_storyLine];
+            ChangeBackGround();
         }
         else
         {
@@ -44,5 +45,17 @@ public class RSJ_EndingStr : MonoBehaviour
         _text.text = _storyList[_storyLine];
         backGround.SetActive(false);
         EndingPan.SetActive(true);
+    }
+
+    private void ChangeBackGround()
+    {
+        if (_storyLine == 2)
+            backGround.GetComponent<Image>().sprite = storyImage[1];
+        else if (_storyLine == 4)
+            backGround.GetComponent<Image>().sprite = storyImage[2];
+        else if (_storyLine == 6)
+            backGround.GetComponent<Image>().sprite = storyImage[3];
+        else if (_storyLine == 7)
+            backGround.GetComponent<Image>().sprite = storyImage[0];
     }
 }
