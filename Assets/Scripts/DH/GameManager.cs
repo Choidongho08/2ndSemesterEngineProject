1f0654792b4721034e6feec8c877b3cb32572631
Choidongho08 <157602117+Choidongho08@users.noreply.github.com>
2024-12-13 00:50:33 +0900

    맵에셋

--- Assets/Scripts/DH/GameManager.cs
+++ Assets/Scripts/DH/GameManager.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.Netcode;
+using Unity.Services.Lobbies.Models;
+using Unity.Services.Lobbies;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -9,59 +11,75 @@ public class GameManager : NetworkBehaviour
 {
     private Canvas _world1Canvas, _world2Canvas;
     private GameObject _world1BackGroundImgManager, _world2BackGroundImgManager;
+    private fdsgnh obj;
+    private string _joinCode;
 
     private void Awake()
     {
         DontDestroyOnLoad(this);
-        MainLobby.instance.OnGameStart += GameStart;
-        SceneManager.sceneLoaded += Gamestart;
+        MainLobby.instance.OnGameStart += GServerRpc;
     }
-    private void GameStart()
+    private void GS()
     {
-        NetworkManager.SceneManager.LoadScene($"Case{NowCase.instance.CaseNumber()}", LoadSceneMode.Single);
-        if (NetworkManager.Singleton.IsConnectedClient)
-        {
-            Debug.Log("Client is connected.");
-        }
-        else
-        {
-            Debug.Log("Client failed to connect to the server.");
-        }
+        GServerRpc();
+
+        //if (IsHost)
+        //{
+        //    GServerRpc();
+        //}
+
+        //if (GameObject.Find("World1Canvas").TryGetComponent(out Canvas canvas)) return;
+        //else
+        //{
+        //    _world1Canvas = GameObject.Find("World1Canvas").GetComponent<Canvas>();
+        //    _world1BackGroundImgManager = GameObject.Find("World1ChangeImgManager");
+        //    _world2Canvas = GameObject.Find("World2Canvas").GetComponent<Canvas>();
+        //    _world2BackGroundImgManager = GameObject.Find("World2ChangeImgManager");
+        //    if (IsHost)
+        //    {
+        //        obj.NetworkObject.Spawn();
+        //        _world2Canvas.gameObject.SetActive(true);
+        //        _world1Canvas.gameObject.SetActive(false);
+        //        _world1BackGroundImgManager.gameObject.SetActive(false);
+        //    }
+        //    else
+        //    {
+        //        _world1Canvas.gameObject.SetActive(true);
+        //        _world2Canvas.gameObject.SetActive(false);
+        //        _world2BackGroundImgManager.gameObject.SetActive(false);
+        //    }
+        //}
+        //if (NetworkManager.Singleton.IsConnectedClient)
+        //{
+        //    Debug.LogError("Client is connected.");
+        //}
+        //else
+        //{
+        //    Debug.LogError("Client failed to connect to the server.");
+        //}
     }
-    private void Gamestart(Scene scene, LoadSceneMode mode)
+    [ServerRpc]
+    private void GServerRpc()
     {
-        if (GameObject.Find("World1Canvas").GetComponent<Canvas>() == null) return;
-        else
-        {
-            GameStartServerRpc();
-            _world1Canvas = GameObject.Find("World1Canvas").GetComponent<Canvas>();
-            _world1BackGroundImgManager = GameObject.Find("World1ChangeImgManager");
-            _world2Canvas = GameObject.Find("World2Canvas").GetComponent<Canvas>();
-            _world2BackGroundImgManager = GameObject.Find("World2ChangeImgManager");
-            if (IsHost)
-            {
-                _world2Canvas.gameObject.SetActive(true);
-                _world1Canvas.gameObject.SetActive(false);
-                _world1BackGroundImgManager.gameObject.SetActive(false);
-            }
-            else if (IsClient)
-            {
-                _world1Canvas.gameObject.SetActive(true);
-                _world2Canvas.gameObject.SetActive(false);
-                _world2BackGroundImgManager.gameObject.SetActive(false);
-            }
-        }
-    }
-    [ServerRpc(RequireOwnership = false)]
-    private void GameStartServerRpc()
-    {
-        GameStartClientRpc();
+        GClientRpc();
     }
     [ClientRpc]
-    private void GameStartClientRpc()
+    private void GClientRpc()
     {
-        fdsgnh obj = Instantiate(GameObject.Find("GameObject").GetComponent<fdsgnh>());
-        obj.NetworkObject.Spawn();
-        Util.instance.LoadingHide();
+        GameStart();
+        //NetworkManager.SceneManager.LoadScene($"Case{NowCase.instance.CaseNumber()}", LoadSceneMode.Single);
+        //obj = Instantiate(GameObject.Find("GameObject").GetComponent<fdsgnh>());
+        //Util.instance.LoadingHide();
+    }
+    private async void GameStart()
+    {
+        if (MainLobby.instance.IsLobbyHost())
+        {
+            _joinCode = await MainRelay.instance.CreateRelay();
+        }
+        else
+        {
+            await MainRelay.instance.JoinCodeRelay(_joinCode);
+        }
     }
 }
